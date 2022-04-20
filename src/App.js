a00ba73824f61d8dca34a9b68afa12c6ede7592c
LeonardoStrechar <strechar21@gmail.com>
2022-04-19 22:03:21 -0300

    adicionado sobre

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css'
 
+
+import curriculo from './docs/curriculo-leonardo-strechar.pdf'
+
 import Developer from './img/Developer.png'
 import SetaParaBaixo from './img/seta-para-baixo.png'
+import FotoDePerfil from './img/FotoDePerfil.jpg'
 
 function App() {
   return (
-    <div>
+    <div className='Main'>
+{/* header*/}
       <header>
         <div className='Title'>
           <h2>PORTFOLIO</h2>
@@ -13,17 +18,19 @@ function App() {
         <div className='Sessions-header'>
           <ul>
             <li>HOME</li>
-            <li>SOBRE MIM</li>
+            <li><a href="#sobre-mim">SOBRE MIM</a></li>
             <li>PROJETOS</li>
             <li>CONTATOS</li>
           </ul>
         </div>
       </header>
+{/* primary session */}
       <div className='Container-home'>
         <div className='Home-img'>
           <img className='Img-developer' src={Developer} alt="Developer"/>
         </div>
         <div className='home-text'>
+          <h4>Olá, eu sou</h4>
           <h1 className='home-text-effect'>Leonardo Strechar</h1>
           <h3>Developer Back-end</h3>
         </div>
@@ -31,16 +38,23 @@ function App() {
       <div className='seta-para-baixo'>
         <img className='seta' src={SetaParaBaixo} alt="Developer"/>
       </div>
-        <div className='Container-sobre-mim'>
-          <div className='sobre-mim-text'>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit eleifend nunc, vulputate sapien varius malesuada diam tristique non nostra integer, interdum feugiat semper dignissim lacus nec facilisi aliquet. Leo vestibulum habitant penatibus quis non lorem pulvinar massa lacinia nullam hac commodo eu, faucibus inceptos dis tortor cras at finibus molestie pretium class sodales. Primis senectus blandit purus ornare ultrices molestie himenaeos, cubilia convallis lectus urna sapien curae pharetra consequat, tortor sociosqu quisque viverra vehicula turpis. Lorem per nullam posuere libero turpis elit vivamus arcu mollis lobortis natoque, parturient duis est ac egestas ligula malesuada mauris eu. Ac fringilla adipiscing sit nisi dis orci proin class varius vel aliquet at sodales, dapibus accumsan praesent aptent aliquam eleifend erat purus luctus duis pulvinar dui, phasellus vitae congue tempus nec fusce metus blandit litora enim ullamcorper conubia. Porttitor montes in placerat elit sagittis arcu facilisis, turpis purus curabitur massa facilisi eget venenatis, ut etiam neque ligula class commodo.
-            </p>
-          </div>
-          <div className='sobre-mim-text'>
-
-          </div>
+{/* second session */}
+      <div id='sobre-mim' className='Container-sobre-mim'>
+        <div className='Container-sobre-mim-text'>
+          <h2>Sobre mim</h2>
+          <p> 
+            Sou Desenvolvedor de sistemas web com mais de 5
+            anos de experiência, desde pequeno sempre fui
+            apaixonado por tecnologia e apesar de nascer no interior e
+            trabalhar por muito tempo com agricultura, nunca deixei
+            essa paixão de lado e hoje virou minha realidade!
+          </p>
+          <button><a href={curriculo} download="Leonardo-strechar.pdf">Curriculo</a></button>
         </div>
+        <div className='Container-sobre-mim-img'>
+          <img className='Img-foto-de-perfil' src={FotoDePerfil} alt="Developer"/>
+        </div>
+      </div>
     </div>
   );
 }
